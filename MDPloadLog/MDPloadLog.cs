2bc4c44b0c35fa60946f62a0577fca07b9c3e29c
Mark <mark@markjacobsen.net>
2025-06-12 15:52:17 -0400

    MDPloadLog

--- MDPloadLog/MDPloadLog.cs
+++ MDPloadLog/MDPloadLog.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CFG2.MDP;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        string loadGuid = Guid.NewGuid().ToString();
+
+        if (args.Length != 1)
+        {
+            MDPLib.Log("Usage: MDPloadLog <full-path-to-file>", loadGuid);
+            return 1;
+        }
+
+        string filePath = args[0];
+
+        if (!File.Exists(filePath))
+        {
+            MDPLib.Log($"File not found: {filePath}", loadGuid);
+            return 1;
+        }
+
+        MDPLib.Log(File.ReadAllText(filePath), loadGuid, false, true);
+        File.Delete(filePath);
+
+        MDPLib.Log($"Loaded {filePath}", loadGuid);
+
+        return 0;
+    }
+}
--- Test/test.bat
+++ Test/test.bat
@@ -4,6 +4,8 @@ if not exist "%SYNC_DRIVE_HOME%\Apps\MDP\Test\" mkdir "%SYNC_DRIVE_HOME%\Apps\MD
 
 xcopy %~dp0\L0_TEST.csv "%SYNC_DRIVE_HOME%\Apps\MDP\Test\" /Y
 
+xcopy %~dp0\LogTest.txt "%SYNC_DRIVE_HOME%\Apps\MDP\Test\" /Y
+
 cd %~dp0
 
 cd ..
@@ -11,3 +13,5 @@ cd ..
 echo %cd%
 
 dotnet run --project MDPloadCSV "%SYNC_DRIVE_HOME%\Apps\MDP\Test\L0_TEST.csv"
+
+dotnet run --project MDPloadLog "%SYNC_DRIVE_HOME%\Apps\MDP\Test\LogTest.txt"
